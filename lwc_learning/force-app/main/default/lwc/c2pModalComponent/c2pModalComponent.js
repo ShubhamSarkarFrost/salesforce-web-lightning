70bf61969b4fc86956a3989221cbfd7cb0ed774d
Shubham Sarkar <ronniecse8@gmail.com>
2025-06-23 01:35:24 +0530

    LWC-Child-To-Parent-Complete-23-06-2025-01:35_am_ist

--- lwc_learning/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ lwc_learning/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,10 +3,15 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement {
     closeHandler(){
         const myCustomEvent = new CustomEvent('close', {
+            bubbles: true,
             detail: {
                 message: "Modal Closed Successfully"
             }
         });
         this.dispatchEvent(myCustomEvent);
+    }
+
+    foorterevent(){
+        console.log("Footer Event Bubbled!!");
     }
 }
